da4f8d2245f550d47bdf42de57822fb373cd6458
Jeon <abc8339265@gmail.com>
2021-10-24 17:27:48 +0900

    project setting & connect to github

---
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/WhatShouldIEatTodayBackApplication.java
@@ -0,0 +1,13 @@
+package com.kj.WhatShouldIEatTodayBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatShouldIEatTodayBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatShouldIEatTodayBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kj/WhatShouldIEatTodayBack/WhatShouldIEatTodayBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kj.WhatShouldIEatTodayBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatShouldIEatTodayBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
