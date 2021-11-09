7881dfb367eb0fb7786b1fb5956a419e062fdbae
Jeon <abc8339265@gmail.com>
2021-11-09 16:28:00 +0900

    make BaseEntity

---
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/domain/BaseEntity.java
@@ -0,0 +1,26 @@
+package com.kj.WhatShouldIEatTodayBack.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(nullable = false, length = 20, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(length = 20)
+    private LocalDateTime updatedAt;
+}
---
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/domain/User.java
@@ -0,0 +1,15 @@
+package com.kj.WhatShouldIEatTodayBack.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+public class User extends BaseEntity{
+
+}
