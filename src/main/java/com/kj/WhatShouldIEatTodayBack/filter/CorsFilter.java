2fa4c1efad3fb6ceda00c3e0afc81d65bda8a498
Jeon <abc8339265@gmail.com>
2022-03-19 15:56:04 +0900

    # modify CORS Filter's url

--- src/main/java/com/kj/WhatShouldIEatTodayBack/filter/CorsFilter.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/filter/CorsFilter.java
@@ -23,7 +23,7 @@ public class CorsFilter implements Filter {
         HttpServletRequest request = (HttpServletRequest) req;
         HttpServletResponse response = (HttpServletResponse) res;
 
-        response.setHeader("Access-Control-Allow-Origin", "https://whatishoudeat.com");
+        response.setHeader("Access-Control-Allow-Origin", "https://whatishouldeat.com");
         response.setHeader("Access-Control-Allow-Credentials", "true");
         response.setHeader("Access-Control-Allow-Methods", "*");
         response.setHeader("Access-Control-Max-Age", "3600");
