836d5c2a2a55fce449d96b8e7d80725da1576393
Jeon <abc8339265@gmail.com>
2022-03-21 19:50:58 +0900

    # test

--- src/main/java/com/kj/WhatShouldIEatTodayBack/api/controller/BasicController.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/api/controller/BasicController.java
@@ -8,6 +8,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class BasicController {
     @RequestMapping(value="/")
     public String hello() {
-        return "Hello!";
+        return "login";
     }
 }
