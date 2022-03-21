c0c0b46b02f40131a4c28f5cf1d8f58597239070
Jeon <abc8339265@gmail.com>
2022-03-21 17:10:41 +0900

    # edit CorsSecurity

--- src/main/java/com/kj/WhatShouldIEatTodayBack/config/security/WebSecurityConfig.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/config/security/WebSecurityConfig.java
@@ -100,9 +100,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 //        configuration.setAllowedOrigins(Arrays.asList("https://whatishouldeat.com", "https://whatishoudeat.com"));
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("*"));
-        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedHeader("*");
+        configuration.addAllowedMethod("*");
         configuration.setAllowCredentials(true);
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
