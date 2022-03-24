11e7552dd75e90bc20974e982301a82b6efd0647
Jeon <abc8339265@gmail.com>
2022-03-24 15:47:02 +0900

    # fix cors

--- src/main/java/com/kj/WhatShouldIEatTodayBack/api/controller/auth/AuthController.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/api/controller/auth/AuthController.java
@@ -7,6 +7,7 @@ import com.kj.WhatShouldIEatTodayBack.dto.memberDTO.MemberResponseDto;
 import com.kj.WhatShouldIEatTodayBack.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
