e2a3080e8e0298871f5c6c94329d7161814e89bb
Jeon <abc8339265@gmail.com>
2022-03-24 18:08:33 +0900

    # edit WebSecurityConfig

--- src/main/java/com/kj/WhatShouldIEatTodayBack/config/security/WebSecurityConfig.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/config/security/WebSecurityConfig.java
@@ -58,7 +58,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                     .defaultSuccessUrl("/api/auth/login/loginSuccess", true)
                     .passwordParameter("memberPw")
                     .usernameParameter("memberEmail")
-                    .successHandler(new SimpleUrlAuthenticationSuccessHandler())
+//                    .successHandler(new SimpleUrlAuthenticationSuccessHandler())
                     .failureHandler(new SimpleUrlAuthenticationFailureHandler()) // 로그인 실패시 401 http Status 반환
                 .and()
                     .requiresChannel() // HTTPS로 로그인 필요
