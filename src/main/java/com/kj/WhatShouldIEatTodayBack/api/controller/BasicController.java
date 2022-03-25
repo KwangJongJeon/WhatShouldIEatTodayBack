ce6e045e31473decf1a53e67693b24a66d0b9aaf
Jeon <abc8339265@gmail.com>
2022-03-25 16:32:42 +0900

    # edit WebSecurityConfig

--- src/main/java/com/kj/WhatShouldIEatTodayBack/api/controller/BasicController.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/api/controller/BasicController.java
@@ -2,9 +2,11 @@ package com.kj.WhatShouldIEatTodayBack.api.controller;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@CrossOrigin(origins = "https://whatishouldeat.com")
 public class BasicController {
     @RequestMapping(value="/")
     public String hello() {
