da7b7344e67968b1defc74ee3a7c0458d252aecb
Jeon <abc8339265@gmail.com>
2022-03-25 18:33:36 +0900

    # edit WebSecurityConfig

--- src/test/java/com/kj/WhatShouldIEatTodayBack/service/AuthServiceTest.java
+++ src/test/java/com/kj/WhatShouldIEatTodayBack/service/AuthServiceTest.java
@@ -1,7 +1,7 @@
-package com.kj.WhatShouldIEatTodayBack.service;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class AuthServiceTest {
-
-}
+//package com.kj.WhatShouldIEatTodayBack.service;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class AuthServiceTest {
+//
+//}
