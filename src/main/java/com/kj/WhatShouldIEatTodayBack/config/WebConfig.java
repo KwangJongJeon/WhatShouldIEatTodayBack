e09b86c938066dd66773accc25feb08f56a09546
Jeon <abc8339265@gmail.com>
2022-03-29 17:37:30 +0900

    # add samesite cookie config

--- src/main/java/com/kj/WhatShouldIEatTodayBack/config/WebConfig.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/config/WebConfig.java
@@ -1,22 +1,23 @@
-//package com.kj.WhatShouldIEatTodayBack.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//@EnableWebMvc
-//public class WebConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry
-//            .addMapping("/**")
-//                .allowedOrigins("https://whatishouldeat.com")
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .allowCredentials(true);
-//
-//    }
-//}
+package com.kj.WhatShouldIEatTodayBack.config;
+
+import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.apache.tomcat.util.http.SameSiteCookies;
+import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+    @Bean
+    public TomcatContextCustomizer sameSiteCookieConfig() {
+        return context -> {
+            final Rfc6265CookieProcessor cookieProcessor = new Rfc6265CookieProcessor();
+            cookieProcessor.setSameSiteCookies(SameSiteCookies.NONE.getValue());
+            context.setCookieProcessor(cookieProcessor);
+        };
+    }
+}
