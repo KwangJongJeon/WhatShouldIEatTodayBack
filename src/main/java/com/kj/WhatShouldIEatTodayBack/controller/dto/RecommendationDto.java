7a92f017101634f9f7a9052b7a9db92b9fa11fb3
Jeon <abc8339265@gmail.com>
2022-12-08 16:53:17 +0900

    feat: 추천 게시판의 카테고리 선택 기능을 HTML에서 Thymeleaf로 마이그레이션했습니다

---
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/controller/dto/RecommendationDto.java
@@ -0,0 +1,19 @@
+package com.kj.WhatShouldIEatTodayBack.controller.dto;
+
+import com.kj.WhatShouldIEatTodayBack.enums.CategoryTypes;
+import lombok.Data;
+
+import java.util.List;
+
+/**
+ * Recommendation 폼에서 사용하기 위한 Dto입니다.
+ * 구성 요소
+ * range: 유저가 탐색을 원하는 범위
+ * categories: 유저가 원하는 음식의 종류(ex, 한식, 중식 양식 등)
+ */
+@Data
+public class RecommendationDto {
+
+    private int range;
+    private List<String> categories;
+}
--- src/main/java/com/kj/WhatShouldIEatTodayBack/controller/recommendation/RecommendationController.java
+++ src/main/java/com/kj/WhatShouldIEatTodayBack/controller/recommendation/RecommendationController.java
@@ -1,20 +1,42 @@
 package com.kj.WhatShouldIEatTodayBack.controller.recommendation;
 
+import com.kj.WhatShouldIEatTodayBack.controller.dto.RecommendationDto;
 import com.kj.WhatShouldIEatTodayBack.domain.member.Member;
+import com.kj.WhatShouldIEatTodayBack.enums.CategoryTypes;
 import com.kj.WhatShouldIEatTodayBack.session.SessionConst;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+@Slf4j
 @Controller
 public class RecommendationController {
 
+    @ModelAttribute("categories")
+    public Map<String, String> categories() {
+        Map<String, String> categories = new LinkedHashMap<>();
+        categories.put("KOREAN", "한식");
+        categories.put("WESTERN", "양식");
+        categories.put("JAPANESE", "일식");
+        categories.put("CHINESE", "중식");
+
+        return categories;
+    }
+
     @GetMapping("/recommendation")
-    public String recommendation(HttpServletRequest request, Model model) {
+    public String recommendation(HttpServletRequest request,
+                                 @ModelAttribute RecommendationDto recommendationDto,
+                                 Model model) {
+
         HttpSession session = request.getSession(false);
 
         if(session != null) {
@@ -26,8 +48,13 @@ public class RecommendationController {
     }
 
     @PostMapping("/recommendation")
-    public String recommendationResult(HttpServletRequest request, Model model) {
+    public String recommendationResult(HttpServletRequest request, @ModelAttribute RecommendationDto recommendationDto, Model model) {
         HttpSession session = request.getSession(false);
+
+        List<String> categories = recommendationDto.getCategories();
+        for (String category : categories) {
+            log.info("category: {}", category);
+        }
 
         if(session != null) {
             Member loginMember = (Member) session.getAttribute(SessionConst.LOGIN_MEMBER);
